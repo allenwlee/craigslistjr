8fcb212751019403f30f9fae369d95639fa1be11
Apprentice <apprentice@dbc03.local>
2013-04-23 19:06:50 -0700

    a ton of changes almost done

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,38 @@
 get '/' do
-  # Look in app/views/index.erb
+  @categories = Category.all
   erb :index
+end
+
+get '/new/category' do
+  erb :new_category
+end
+
+post '/new/category' do
+  Category.create(params[:category])
+
+  redirect '/'
+end
+
+get '/category/:name' do
+  category_name = params[:name]
+  @category = Category.where("name = ?", category_name).first
+  erb  :category
+end
+  
+post '/new/post' do
+  @post = Post.create(params[:post])
+
+  #c = Category.find(params[:category_id])
+  #c.posts << Post.create(params[:post])
+  redirect "/category/#{@post.category.name}"
+end
+
+get '/:id/edit' do
+  @post = Post.find(params[:id])
+  erb  :edit
+end
+
+post '/:id/edit' do
+  
+  @post.update_attributes(params[:post])
 end
---
+++ db/migrate/20130423152441_create_posts_table.rb
@@ -0,0 +1,11 @@
+class CreatePostsTable < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :description
+      t.string :email
+      t.string :price
+      t.references :category
+    end
+  end
+end
